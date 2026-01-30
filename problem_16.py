7f959f0ed5d8ac0765a89a67c838e3b7ced5d748
Ajay Rahul <ajaypriya200825@gmail.com>
2026-01-30 15:38:08 +0530

    changed numbers

--- problem_16.py
+++ problem_16.py
@@ -1,7 +1,7 @@
 # Problem 16: Find the second largest number in a list
 # Find and fix the error
 
-numbers = [1,2,3,4,5]
+numbers =[1,2,3,4,5]
 numbers.sort()
 second_largest = numbers[-2]
 print(f"Second largest: {second_largest}")
