f8da81b257203cd0f6c026973936b8b0c5d6105f
Ajay Rahul <ajaypriya200825@gmail.com>
2026-01-30 15:08:21 +0530

    Racecar to racecar

--- problem_08.py
+++ problem_08.py
@@ -6,6 +6,6 @@ def is_palindrome(text):
 
 word = "racecar"
 if is_palindrome(word):
-    print(f"{word} is a palindrome")
+    print(f"{word} is  a palindrome")
 else:
     print(f"{word} is not a palindrome")
